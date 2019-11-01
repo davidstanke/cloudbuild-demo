452c0127a123272d991a92f45c7eb865f941d581
Dave Stanke <davidstanke@google.com>
2019-11-01 12:36:46 -0400

    Implement web call to shipping cost API

--- src/shippingcalc/app.js
+++ src/shippingcalc/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var shippingCostRouter = require('./routes/shippingcost');
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/shippingcost/', shippingCostRouter);
