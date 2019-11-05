24b5300e6e69cb354fecfcc7fd5931a0629caa78
Dave Stanke <davidstanke@google.com>
2019-11-05 15:24:55 -0500

    add k8s configs for local dev

--- tools/deploy-local-k8s.sh
+++ tools/deploy-local-k8s.sh
@@ -3,17 +3,41 @@ set -eo pipefail
 
 GCP_PROJECT_ID="project_undefined"
 
+# ============ BEGIN: parse flags ================
+
+OPTS=`getopt -o vhns: --long verbose,dry-run,help,stack-size: -n 'parse-options' -- "$@"`
+if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+
+SKIP_BUILD="false"
+
+while true; do
+  case "$1" in
+    -s | --skip-build ) SKIP_BUILD="true"; shift ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+# ============ END: parse flags ================
+
 if hash gcloud 2>/dev/null; then
     GCP_PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 fi
 
-echo $GCP_PROJECT_ID
+echo "Using GCP Project ID $GCP_PROJECT_ID"
 
-docker build -t \
-    gcr.io/$GCP_PROJECT_ID/cookieshop/shippingcalc:local \
-    --build_arg PORT=8080 \
-    ../src/shippingcalc
+if [ "$SKIP_BUILD" = "false" ] ; then
+    docker build -t \
+        gcr.io/$GCP_PROJECT_ID/cookieshop/shippingcalc:local \
+        --build-arg PORT=8080 \
+        ../src/shippingcalc
+    docker build -t \
+        gcr.io/$GCP_PROJECT_ID/cookieshop/web:local \
+        --build-arg PORT=8080 \
+        ../src/web
+fi
 
-kubectl delete namespce local || true
+# reinitialize 'local' namespace
+kubectl delete namespace local --ignore-not-found
 kubectl create namespace local
-kubectl apply -f k8s-local.yml
+kubectl apply -f k8s-local/ --namespace=local
