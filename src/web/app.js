7c0355809e7a69eef89316f46ce38058b627902f
Dave Stanke <davidstanke@gmail.com>
2019-11-20 16:50:03 -0800

    Target cloud run (#1)
    This PR creates a completely new app which is intended for running on Cloud Run

--- src/web/app.js
+++ src/web/app.js
@@ -1,26 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const path = require('path');
+const express=require('express');
+const createError = require('http-errors');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const port=8080; // TODO: allow override via env var
 
-var app = express();
-
-// view engine setup
+var app=express();
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// register routes
+app.get('/', async (req, res) => {
+    res.render('index', { 
+      title: "Hello from Google Cloud",
+      greeting: "Hello from Google Cloud",
+      banner: "/img/logo_cloud_icon.png",
+      bannerUrl: "https://cloud.google.com"
+    });
+  });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.all('/greet', async (req, res) => {
+    let user=req.body.name;
+    res.render('default', { 
+        title: "Hello, " + user,
+        greeting: "Hello, " + user + "! We think you're super.",
+        banner: "/img/logo_cloud_icon.png",
+        bannerUrl: "https://cloud.google.com"
+    });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +45,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  next();
 });
 
-module.exports = app;
+// start the server
+var server=app.listen(port, function() {});
+
+module.exports = server;
---
+++ src/web/tests/app.test.js
@@ -0,0 +1,45 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const app = require('../app');
+chai.use(chaiHttp);
+
+describe('server', () => {
+    it('should be listening', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err,res) => {
+                expect(res.status).to.equal(200);
+                done();
+            })
+    });
+
+    it('should display the homepage', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err,res) => {
+                expect(res.text).to.contain("Hello");
+                done();
+            })
+    })
+
+    it('should display a personalized greeting', (done) => {
+        chai.request(app)
+            .post('/greet')
+            .type('form')
+            .send({'name':'Dave'})
+            .end((err,res) => {
+                expect(res.text).to.contain("Hello, Dave!");
+                done();
+            })
+    })
+});
+
+
+
+// describe('app', () =>{
+//     it('should return the homepage greeting', () => {
+//         var greeting=app.getPageContent('/');
+//         expect(greeting).to.contain('Hello');
+//     })
+// });
