d96184c05a4015ad7007ad4a3684730021304d58
Dave Stanke <davidstanke@google.com>
2019-11-27 15:07:52 -0500

    amend workshoperize script

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:12
+FROM node:12-alpine
 COPY . .
 RUN npm install --only=production
 CMD npm start
--- _workshop/prepare_workshop.sh
+++ _workshop/prepare_workshop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eou pipefail
 
-# To prepare a "workshop" branch with incomplete config, checkout master and run this script
+# To prepare a "workshop" branch with incomplete config, checkout master and run this script from the project root
 
 # delete workshop branch if it exists
 git branch -d workshop &>/dev/null || true
 git checkout -b workshop
 
 # remove Dockerfile answers
-sed -i 's/FROM node:12/FROM ???/g' Dockerfile
+sed -i '' 's/FROM node:12/FROM ???/g' Dockerfile
