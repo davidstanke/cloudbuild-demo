ab3e2aeb425cd4bd0847e46c624f880262b42a64
Dave Stanke <davidstanke@google.com>
2019-11-27 15:03:57 -0500

    docker workshoperize

--- _workshop/prepare_workshop.sh
+++ _workshop/prepare_workshop.sh
@@ -6,3 +6,6 @@ set -eou pipefail
 # delete workshop branch if it exists
 git branch -d workshop &>/dev/null || true
 git checkout -b workshop
+
+# remove Dockerfile answers
+sed -i 's/FROM node:12/FROM ???/g' Dockerfile
