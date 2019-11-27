8395dfa09cb12990bd7f97c93559ec161a777221
Dave Stanke <davidstanke@google.com>
2019-11-27 15:22:17 -0500

    amend workshoperize script

--- _workshop/prepare_workshop.sh
+++ _workshop/prepare_workshop.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -eou pipefail
 
-# To prepare a "workshop" branch with incomplete config, checkout master and run this script from the project root
+# To prepare a "workshop" branch with incomplete config, checkout master and run this script from the _workshop folder
 
 # delete workshop branch if it exists
 git branch -d workshop &>/dev/null || true
 git checkout -b workshop
 
 # remove Dockerfile answers
-sed -i '' 's/node:12-alpine/???/g' Dockerfile
-sed -i '' 's/npm install --only=production/???/g' Dockerfile
-sed -i '' 's/npm start/???/g' Dockerfile
-sed -i '' 's/8080/???/g' Dockerfile
+mv -f Dockerfile_stripped ../Dockerfile
+
+# remove cloudbuild.yaml answers
