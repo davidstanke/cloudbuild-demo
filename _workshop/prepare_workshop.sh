36ba70685fe797ff8071a93f9923468407cc966d
Dave Stanke <davidstanke@google.com>
2019-11-27 15:36:50 -0500

    change workshopify script to run from repo root

--- _workshop/prepare_workshop.sh
+++ _workshop/prepare_workshop.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 set -eou pipefail
 
-# To prepare a "workshop" branch with incomplete config, checkout master and run this script from the _workshop folder
+# To prepare a "workshop" branch with incomplete config, checkout master and run this script from the repo root
 
 # delete workshop branch if it exists
 git branch -d workshop &>/dev/null || true
 git checkout -b workshop
 
 # remove Dockerfile answers
-cp -f Dockerfile_stripped ../Dockerfile
+cp -f _workshop/Dockerfile_stripped Dockerfile
 
 # remove cloudbuild.yaml answers
-cp -f cloudbuild_stripped.yaml ../cloudbuild.yaml
+cp -f _workshop/cloudbuild_stripped.yaml cloudbuild.yaml
 
 # break a test
-sed -i '' 's/Hello, Dave/Hi, Dave/g' ../tests/app.test.js
+sed -i '' 's/Hello, Dave/Hi, Dave/g' tests/app.test.js
 
 # add a linting error
-sed -i '' 's/port=8080;/port=8080;;/g' ../app.js
+sed -i '' 's/port=8080;/port=8080;;/g' app.js
+
+# delete this directory
+rm -rf _workshop
