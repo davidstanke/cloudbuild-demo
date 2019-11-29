e9bb2ff063d29f9bb1f66ce77d1419dbd34041ff
Dave Stanke <davidstanke@google.com>
2019-11-29 14:27:02 -0500

    change git prep

--- _workshop/prepare_workshop.sh
+++ _workshop/prepare_workshop.sh
@@ -4,7 +4,8 @@ set -eou pipefail
 # To prepare a "workshop" branch with incomplete config, checkout master and run this script from the repo root
 
 git pull
-git checkout workshop || git checkout -b workshop
+git branch -d workshop || true
+git checkout -b workshop
 git branch --set-upstream-to=origin/workshop
 
 # remove Dockerfile answers
